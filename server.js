0a18e16789dc2bd97dd9b2d2d26dbc54c3a5e96f
WASEEM KHAN <waseem3khan333@gmail.com>
2024-01-17 22:07:59 +0500

    initial commit

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import authRouters from './routes/authRoute.js';
 import categoryRoute from './routes/categoryRoute.js'
 import productRoute from './routes/productRoute.js'
 import cors from 'cors'
+import path from 'path'
 dotenv.config();
 const app = express();
 
@@ -14,18 +15,18 @@ connectiondb();
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(express.static(path.json(__dirname,'./client/build')))
+
 // routers
 app.use('/api/v1/auth',authRouters)
 app.use('/api/v1/category',categoryRoute)
 app.use('/api/v1/product',productRoute)
 // API
-app.get('/',(req,res)=>{
-    res.send({
-        message:"Welcome to ecmmerce app",
-    });
+app.use('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'./client/build/index.html'))
 })
 // create server
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080;  
 app.listen(port,()=>{
     console.log(`App running successfully on ${process.env.DEV_NODE}  mode  on ${port} port.`)
 })
